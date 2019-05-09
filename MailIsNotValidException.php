f2cdc9f48c2d0d02b69f0c9931b9bcaee7215db7
sedlak <slavomir.sedlak@ringier.sk>
2019-05-09 13:45:42 +0200

    add mail sender

---
+++ MailIsNotValidException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slavomir.sedlak
+ * Date: 2019-05-06
+ * Time: 16:33
+ */
+
+namespace Mail_sender\Exceptions;
+
+
+class MailIsNotValidException extends \Exception
+{
+
+}
---
+++ SendMailService.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slavomir.sedlak
+ * Date: 2019-05-06
+ * Time: 16:15
+ */
+
+namespace App\Services;
+
+
+use Mail_sender\Exceptions\MailIsNotValidException;
+
+class SendMailService
+{
+    /**
+     * @param string $mail
+     * @param string $head
+     * @param string $mailContent
+     * @throws MailIsNotValidException
+     * @return string
+     */
+    public function send(string $mail, string $head, string $mailContent): string
+    {
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            throw new MailIsNotValidException('Mail is not in valid format');
+        }
+        //mail($mail, $head,$mailContent);
+        return true;
+    }
+}
